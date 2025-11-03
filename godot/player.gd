7076f0ca3a4636c5bfe4e755428d5ee531790341
luSteven01 <146397397+luSteven01@users.noreply.github.com>
2025-11-03 01:58:56 -0800

    finished living_room foundation

---
+++ godot/player.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+@export var speed = 100
+
+func _physics_process(delta: float) -> void:
+	var direction = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_down"):
+		direction.y += 1
+	if Input.is_action_pressed("move_up"):
+		direction.y -= 1
+		
+	if direction.length() > 0:
+		direction = direction.normalized()
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		
+	if direction.x < 0: # Moving Left
+		$AnimatedSprite2D.animation = "walk_left"
+	elif direction.x > 0: # Moving Right
+		$AnimatedSprite2D.animation = "walk_right"
+	elif direction.y < 0: # Moving Up (only if not moving left or right)
+		$AnimatedSprite2D.animation = "walk_up"
+	elif direction.y > 0: # Moving Down (only if not moving left or right)
+		$AnimatedSprite2D.animation = "walk_down"
+	
+	velocity = direction * speed
+
+	move_and_slide()
