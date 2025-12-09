fefd2a1b4d196311692cd70de51cb008f13daf2e
Pikalot <pikalot@hotmail.com>
2025-12-09 04:37:09 -0800

    Cleaned up main.gd unused codes

--- godot/main.gd
+++ godot/main.gd
@@ -2,16 +2,11 @@ extends Node
 
 @onready var current_level_node = $WorldContext/CurrentLevel
 @onready var player = $WorldContext/Player
-
+@onready var main_menu_scene = $CanvasLayer/MainMenu
 @export var starting_scene: PackedScene
 @export var starting_position: Vector2 = Vector2(127, 213)
 
 func _ready():
-	# $WorldContext.visible = false
-	# player.hide()
-	# SceneManager.level_change_requested.connect(_on_level_change_requested)
-	# get_viewport().set_input_as_handled()
-
 	# Hide world
 	$WorldContext.visible = false
 	player.hide()
@@ -20,10 +15,7 @@ func _ready():
 
 	# Freeze all gameplay
 	get_tree().paused = true
-	$CanvasLayer/MainMenu.process_mode = Node.PROCESS_MODE_WHEN_PAUSED
-
-	# Allow MainMenu UI to still receive input
-	# $CanvasLayer/MainMenu.pause_mode = Node.PAUSE_MODE_PROCESS
+	main_menu_scene.process_mode = Node.PROCESS_MODE_WHEN_PAUSED
 
 
 func _on_level_change_requested(new_scene_packed: PackedScene, new_pos: Vector2):
@@ -59,18 +51,12 @@ func _perform_level_change(new_scene_packed: PackedScene, new_pos: Vector2):
 
 func _on_main_menu_start_game() -> void:
 	# hide menu
-	$CanvasLayer/MainMenu.visible = false
+	main_menu_scene.visible = false
 		
 	# show world
 	$WorldContext.visible = true
 	player.show()
 	get_tree().paused = false
-	
-	
-	#if starting_scene == null:
-		#starting_scene = load("res://living_room.tscn")
-	#
-	
+
 	# Load the first level immediately (safe to do here)
 	_perform_level_change(starting_scene, starting_position)
-	# GameManager.init_dialogue_box($CanvasLayer/DialogueBox)
